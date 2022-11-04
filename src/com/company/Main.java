07e079abff540cdd75d989f54096298598c34698
hooda.armaan <armaan.hooda040406@gmail.com>
2022-11-04 18:13:38 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Q:2 - Encapsulation refers to the bundling of fields and methods inside a single class. It is a key concept in OOP.");
+        System.out.println("Q:3 - byte, short, int, long, double, float");
+        System.out.println("Q:4 - The fundamental difference is of the storage size: int is 32 bits in width while long is 64 bits in width.\n      Another difference is the range of values that each type permits: int stores whole numbers from -(2^31 - 1) to 2^31 - 1 while long stores\n      whole numbers from -(2^63 - 1) to 2^63 - 1");
+        System.out.println("Q:5 - A field is a member variable that belongs to a class, whereas a method is a function or a set of commands referred to by name, bound to an\n      object of the class.");
+    }
+}
